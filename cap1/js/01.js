be284e297bbfe4a4bd74471c9ba0470ea5a48650
Édipo Nascimento <esnedipo@gmail.com>
2021-09-26 16:51:11 -0300

    using the console to display data, nad nodejs readline to read data from command line

---
+++ cap1/js/01.js
@@ -0,0 +1,22 @@
+// declaring variable
+var age = 18;
+
+console.log(`You age is: ${age}`);
+
+// readind data from the keyboard. I serched on the internet and found an article talking about Nodejs.
+// So, I used it.
+
+const readline = require('readline');
+
+const input = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+input.question(`Whta is your name? `, name => {
+  if (name) {
+    console.log(`Hi ${name}`);
+    input.close()
+  }
+  input.close();
+})
