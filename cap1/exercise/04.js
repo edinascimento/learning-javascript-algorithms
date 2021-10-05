fcdb53a92a3518c5dfeb1029a59f6f8ef8108c50
Édipo Nascimento <esnedipo@gamil.com>
2021-10-05 13:55:04 -0300

    Using vectores to store suers data

---
+++ cap1/exercise/04.js
@@ -0,0 +1,12 @@
+// vetor
+
+var programmingLanguages = []
+
+programmingLanguages[0] = 'Java';
+programmingLanguages[1] = 'Python';
+programmingLanguages[2] = 'Ruby';
+programmingLanguages[3] = 'Golang';
+
+programmingLanguages.forEach(lagnauge => {
+  console.log(`Programming Language: ${lagnauge}`);
+});
